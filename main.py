40350f8cf2aa52d7d30ceb525e540a9fa3cd342d
toozigbapow <31927322+srenville@users.noreply.github.com>
2020-06-13 18:26:49 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,20 @@
+import bluetooth
+
+target_name = "OneDer V3"
+target_address = None
+
+
+nearby_devices = bluetooth.discover_devices()
+
+print (nearby_devices)
+
+for bdaddr in nearby_devices:
+    print(bluetooth.lookup_name( bdaddr ))
+    if target_name == bluetooth.lookup_name( bdaddr ):
+        target_address = bdaddr
+        break
+
+if target_address is not None:
+    print ("found target bluetooth device with address ", target_address)
+else:
+    print ("could not find target bluetooth device nearby")
